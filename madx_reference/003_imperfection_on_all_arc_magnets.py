ebc3f091f28a5408592a9667c9d00f7203f6ec8d
giadarol <giovanni.iadarola@cern.ch>
2026-03-01 21:58:56 +0100

    Errors applied, to be tested

--- madx_reference/003_imperfection_on_all_arc_magnets.py
+++ madx_reference/003_imperfection_on_all_arc_magnets.py
@@ -91,3 +91,40 @@ for ii in range(0, max_order):
 
 tt_err_two_aper['knl_rel'] = knl_rel
 tt_err_two_aper['ksl_rel'] = ksl_rel
+
+multipole_errors = {}
+for nn in tt_err_two_aper.name:
+    knl_rel = tt_err_two_aper['knl_rel', nn]
+    ksl_rel = tt_err_two_aper['ksl_rel', nn]
+    ref_order = tt_err_two_aper['ref_order', nn]
+    multipole_errors[nn] = {'knl_rel': knl_rel, 'ksl_rel': ksl_rel, 'ref_order': ref_order}
+
+# Suppress multipoles of order < 2
+min_order = 2
+for nn in multipole_errors:
+    multipole_errors[nn]['knl_rel'][:min_order] = 0
+    multipole_errors[nn]['ksl_rel'][:min_order] = 0
+
+# Apply errors in the line
+for nn in line.element_names:
+    if not hasattr(line[nn], 'knl'):
+        continue  # skip non-multipoles
+
+    print(f'Applying errors to {nn}               ', end='\r', flush=True)
+    nn_err = nn.split('..')[0]  # remove ..1, ..2, etc.
+    if nn_err in multipole_errors:
+        line.extend_knl_ksl(order=max_order, element_names=[nn])
+        for ii in range(min_order, max_order):
+            kknn_rel = multipole_errors[nn_err]['knl_rel'][ii]
+            kkss_rel = multipole_errors[nn_err]['ksl_rel'][ii]
+            ref_order = multipole_errors[nn_err]['ref_order']
+            if ii == ref_order:
+                raise ValueError(
+                    f"Error of order {ii} for {nn} is relative to the reference multipole, which is not supported. "
+                )
+            line.ref[nn].knl[ii] += kknn_rel * line.ref[nn].knl[ref_order]
+            line.ref[nn].ksl[ii] += kkss_rel * line.ref[nn].knl[ref_order]
+            # line.get(nn).knl[ii] = kknn_rel * line.get(nn).knl[ref_order]
+            # line.get(nn).ksl[ii] = kkss_rel * line.get(nn).knl[ref_order]
+
+line.to_json('test_line_with_errors.json')
