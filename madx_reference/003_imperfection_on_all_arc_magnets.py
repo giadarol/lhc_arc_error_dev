b34f1808851f0a3cb0a07d8fdfae6133ef46f138
giadarol <giovanni.iadarola@cern.ch>
2026-03-01 17:28:43 +0100

    Massage the error table

--- madx_reference/003_imperfection_on_all_arc_magnets.py
+++ madx_reference/003_imperfection_on_all_arc_magnets.py
@@ -10,6 +10,7 @@ tt_err_data = tt_raw._data.copy()
 tt_err_data['name'] = np.array([nn.lower() for nn in tt_err_data['name']])
 tt_err = xt.Table(data=tt_err_data, col_names=tt_raw._col_names)
 
+# dicts with LHC geography
 B1_INSIDE = {'b1': 'v2', 'b2': 'v1',
              'v1': 'b2', 'v2': 'b1',
              'internal_beam': 'b1', 'external_beam': 'b2',
@@ -31,3 +32,33 @@ BEAM_MAPPING_PER_SIDE = {
     'r7': B1_OUTSIDE, 'l8': B1_OUTSIDE,
     'r8': B1_INSIDE,  'l1': B1_INSIDE,
 }
+
+SIDE_APER_TO_SIDE_BEAM = {}
+for side in BEAM_MAPPING_PER_SIDE:
+    for aper in ['v1', 'v2']:
+        SIDE_APER_TO_SIDE_BEAM[f"{side}.{aper}"] = f"{side}.{BEAM_MAPPING_PER_SIDE[side][aper]}"
+
+
+# Isolate magnets with two apertures (end with .b1, .b2, .v1, .v2)is_two_aper = np.array([nn.endswith('.b1') or nn.endswith('.b2') or nn.endswith('.v1') or nn.endswith('.v2') for nn in line.element_names])
+tt_two_aper = tt_err.rows[r'.*\.b1|.*\.b2|.*\.v1|.*\.v2']
+
+# Attach name with beam instead of aperture to error table
+
+name_with_aper = tt_two_aper['name']
+name_with_beam = np.array([SIDE_APER_TO_SIDE_BEAM.get(nn, nn) for nn in name_with_aper])
+tt_two_aper['name_with_aper'] = name_with_aper
+tt_two_aper['name_with_beam'] = name_with_beam
+tt_two_aper['name'] = name_with_beam
+
+
+tt = line.get_table()
+name_no_slice = []
+slice_index = []
+for nn in tt.name:
+    if '..' in nn and not nn.endswith('..r'): # ignore the "r" correctors ?!
+        nn_no_slice = nn.split('..')[0]
+        name_no_slice.append(nn_no_slice)
+        slice_index.append(int(nn.split('..')[1]))
+    else:
+        name_no_slice.append(nn)
+        slice_index.append(None)
